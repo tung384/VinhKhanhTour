bedf1f926bb389320681c42d9537a298d5e492a2
Nguyễn Tùng <0978938301a@gmail.com>
2026-03-25 10:48:45 +0700

    3/25/2026

--- VinhKhanhTour/MauiProgram.cs
+++ VinhKhanhTour/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui; // Thêm Toolkit để hỗ trợ UI tốt hơn
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 
 namespace VinhKhanhTour;
 
@@ -9,12 +12,17 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiMaps()
+            .UseMauiCommunityToolkit() // Kích hoạt Toolkit
+            .UseMauiMaps() // BẮT BUỘC: Để bản đồ hiển thị được
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        // Đăng ký các Page dưới dạng Singleton để tiết kiệm bộ nhớ khi dùng SQL
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<WebReviewPage>();
 
 #if DEBUG
         builder.Logging.AddDebug();
@@ -23,4 +31,3 @@ public static class MauiProgram
         return builder.Build();
     }
 }
-//END
---
+++ VinhKhanhTour/WebReviewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace VinhKhanhTour;
+
+public partial class WebReviewPage : ContentPage
+{
+    public WebReviewPage() { InitializeComponent(); }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (Application.Current?.MainPage is Shell shell && shell.CurrentPage is MainPage main)
+            BindingContext = main;
+    }
+
+    private async void OnAddressTapped(object sender, EventArgs e)
+    {
+        if (sender is Label lb && lb.GestureRecognizers[0] is TapGestureRecognizer tap && tap.CommandParameter is POI poi)
+            await Microsoft.Maui.ApplicationModel.Map.Default.OpenAsync(poi.Location, new MapLaunchOptions { Name = poi.Name });
+    }
+
+    private async void OnSpeakClicked(object sender, EventArgs e)
+    {
+        if (sender is Button b && b.CommandParameter is POI poi && !string.IsNullOrWhiteSpace(poi.DescriptionVi))
+            await TextToSpeech.Default.SpeakAsync(poi.DescriptionVi);
+    }
+}
